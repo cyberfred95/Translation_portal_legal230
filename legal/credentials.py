dccd987290460f32a2418ab2d267d25d683af499
Andrew Khlopot <a.xlopot@gmail.com>
2023-12-12 11:12:13 +0200

    add credentials for modernMT provider

--- legal/credentials.py
+++ legal/credentials.py
@@ -128,24 +128,105 @@ provider_models = {
         }
     },
     'ModernMT': {
-        'mmt_fr_en_litiges': {
-            'title': 'Litiges',
-            'title_fr': 'Litiges',
-            'provider': 'mmt',
-            'category_id': 'c8e195f8-55ca-407d-906d-b52201cd219d-LAW',
-            'key': 'REMOVED_MMT_KEY',
-            'source_lng': 'en',
-            'target_lng': 'fr'
-        },
-        'mmt_en_fr_litiges': {
-            'title': 'Litiges',
-            'title_fr': 'Litiges',
-            'provider': 'mmt',
-            'category_id': 'c8e195f8-55ca-407d-906d-b52201cd219d-LAW',
-            'key': 'REMOVED_MMT_KEY',
-            'source_lng': 'fr',
-            'target_lng': 'en'
-        }
+    'mmt_en_fr_des': {
+        'title': 'Droit des Sociétés',
+        'title_fr': 'Droit des Sociétés',
+        'provider': 'mmt',
+        'category_id': '4aaaeeba-cf81-4c57-8d82-ab5cec9aa4ca-LAW',
+        'key': 'REMOVED_MMT_KEY',
+        'source_lng': 'en',
+        'target_lng': 'fr'
+    },
+    'mmt_fr_en_des': {
+        'title': 'Droit des Sociétés',
+        'title_fr': 'Droit des Sociétés',
+        'provider': 'mmt',
+        'category_id': '4aaaeeba-cf81-4c57-8d82-ab5cec9aa4ca-LAW',
+        'key': 'REMOVED_MMT_KEY',
+        'source_lng': 'fr',
+        'target_lng': 'en'
+    },
+    'mmt_en_fr_immobilier': {
+        'title': 'Droit Immobilier',
+        'title_fr': 'Droit Immobilier',
+        'provider': 'mmt',
+        'category_id': '3823a279-8776-4b21-a7ed-c0d6d83379b3-LAW',
+        'key': 'REMOVED_MMT_KEY',
+        'source_lng': 'en',
+        'target_lng': 'fr'
+    },
+    'mmt_en_fr_social': {
+        'title': 'Droit social',
+        'title_fr': 'Droit social',
+        'provider': 'mmt',
+        'category_id': '67bd74ac-cd32-4455-8749-efe8c9ce6698-LAW',
+        'key': 'REMOVED_MMT_KEY',
+        'source_lng': 'en',
+        'target_lng': 'fr'
+    },
+    'mmt_fr_en_social': {
+        'title': 'Droit social',
+        'title_fr': 'Droit social',
+        'provider': 'mmt',
+        'category_id': '67bd74ac-cd32-4455-8749-efe8c9ce6698-LAW',
+        'key': 'REMOVED_MMT_KEY',
+        'source_lng': 'fr',
+        'target_lng': 'en'
+    },
+    'mmt_en_fr_financier': {
+        'title': 'Droit financier',
+        'title_fr': 'Droit financier',
+        'provider': 'mmt',
+        'category_id': '1f67a0ef-c4d0-425c-a2d4-2c83f0213ce6-LAW',
+        'key': 'REMOVED_MMT_KEY',
+        'source_lng': 'en',
+        'target_lng': 'fr'
+    },
+    'mmt_fr_en_financier': {
+        'title': 'Droit financier',
+        'title_fr': 'Droit financier',
+        'provider': 'mmt',
+        'category_id': '1f67a0ef-c4d0-425c-a2d4-2c83f0213ce6-LAW',
+        'key': 'REMOVED_MMT_KEY',
+        'source_lng': 'fr',
+        'target_lng': 'en'
+    },
+    'mmt_en_fr_commercial': {
+        'title': 'Droit commercial',
+        'title_fr': 'Droit commercial',
+        'provider': 'mmt',
+        'category_id': '3749e601-f2e4-4c58-aee5-40ab57cfea3d-LAW',
+        'key': 'REMOVED_MMT_KEY',
+        'source_lng': 'en',
+        'target_lng': 'fr'
+    },
+    'mmt_fr_en_commercial': {
+        'title': 'Droit commercial',
+        'title_fr': 'Droit commercial',
+        'provider': 'mmt',
+        'category_id': '3749e601-f2e4-4c58-aee5-40ab57cfea3d-LAW',
+        'key': 'REMOVED_MMT_KEY',
+        'source_lng': 'fr',
+        'target_lng': 'en'
+    },
+    'mmt_en_fr_litiges': {
+        'title': 'Litiges',
+        'title_fr': 'Litiges',
+        'provider': 'mmt',
+        'category_id': 'c8e195f8-55ca-407d-906d-b52201cd219d-LAW',
+        'key': 'REMOVED_MMT_KEY',
+        'source_lng': 'en',
+        'target_lng': 'fr'
+    },
+    'mmt_fr_en_litiges': {
+        'title': 'Litiges',
+        'title_fr': 'Litiges',
+        'provider': 'mmt',
+        'category_id': '4aaaeeba-cf81-4c57-8d82-ab5cec9aa4ca-LAW',
+        'key': 'REMOVED_MMT_KEY',
+        'source_lng': 'fr',
+        'target_lng': 'en'
+    },
     }
 
 }
