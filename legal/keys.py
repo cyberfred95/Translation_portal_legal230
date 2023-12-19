219833cc5df3eba2b10a37eab2633e5e4eede43e
Andrew Khlopot <a.xlopot@gmail.com>
2023-12-19 17:59:07 +0200

    move credentials to a specific file

--- legal/keys.py
+++ legal/keys.py
@@ -1,2 +1,5 @@
 MS_KEY = 'REMOVED_MS_KEY'
 MMT_KEY = 'REMOVED_MMT_KEY'
+MS_ACCESS_TOKEN = 'REMOVED_AZURE_KEY_1'
+MS_CONNECTION = f'DefaultEndpointsProtocol=https;AccountName=legal230storage;AccountKey={MS_ACCESS_TOKEN};EndpointSuffix=core.windows.net'
+MS_ENDPOINT = 'https://legal230storage.blob.core.windows.net/'
