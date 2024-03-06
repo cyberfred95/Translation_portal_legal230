32f659245ef681ced977592d4952a66fa312c763
Andrew Khlopot <a.xlopot@gmail.com>
2024-03-06 17:00:30 +0200

    Update mail_helpers.py

--- legal/mail_helpers.py
+++ legal/mail_helpers.py
@@ -3,13 +3,14 @@ from users.models import User
 from django.conf import settings
 from django.template.loader import render_to_string
 from sendgrid.helpers.mail import (Mail, Attachment, FileContent, FileName, FileType, Disposition)
-
+from preferences import preferences
 
 def send_text_translation(
         user_id,
         template_name=None,
         file_ext=None,
         text=None,
+        action=None,
         theme='Text translation',
         attachment=None,
         file_name=None,
@@ -26,16 +27,15 @@ def send_text_translation(
     for item in users_to_send:
         emails.append(item.email)
 
-    for user_to_send in users_to_send:
-
+    if action == 'expert_revision':
         message = Mail(
             from_email='support@custom.mt',
-            to_emails=[user_to_send.email],
+            to_emails=[preferences.MainSettings.sender_email],
             subject=theme,
             html_content=render_to_string(
                 template,
                 {
-                    "user_name": user_to_send.username,
+                    "user_name": preferences.MainSettings.sender_email,
                     "text": text,
                     "template_name": template_name,
                     "sender_username": user.username,
@@ -44,20 +44,41 @@ def send_text_translation(
                 }
             )
         )
-        if attachment and file_name:
-            attachedFile = Attachment(
-                file_content=FileContent(attachment),
-                file_name=FileName(file_name),
-                # FileType('application/pdf'),
-                disposition=Disposition('attachment')
-            )
-            message.attachment = attachedFile
         sg.send(message)
+
+    else:
+        for user_to_send in users_to_send:
+
+            message = Mail(
+                from_email='support@custom.mt',
+                to_emails=[user_to_send.email],
+                subject=theme,
+                html_content=render_to_string(
+                    template,
+                    {
+                        "user_name": user_to_send.username,
+                        "text": text,
+                        "template_name": template_name,
+                        "sender_username": user.username,
+                        "file_ext": file_ext,
+                        "file_url": file_url
+                    }
+                )
+            )
+            if attachment and file_name:
+                attachedFile = Attachment(
+                    file_content=FileContent(attachment),
+                    file_name=FileName(file_name),
+                    # FileType('application/pdf'),
+                    disposition=Disposition('attachment')
+                )
+                message.attachment = attachedFile
+            sg.send(message)
 
 
 def send_file_translation(user_id, file_url, file_name, file_ext, template_name):
     send_text_translation(user_id=user_id, theme='File translation', file_url=file_url, file_name=file_name,
-                          template_name=template_name, file_ext=file_ext, template='expert_revision_email.html')
+                          template_name=template_name, file_ext=file_ext, template='file_email.html')
 
 
 def send_gpt_processing(user_id, text):
@@ -69,7 +90,9 @@ def send_expert_revision_text(user_id, text):
         user_id=user_id,
         text=text,
         theme='Revision request for Text translation',
-        template="expert_revision_email.html",
+        template="expert_revision_text.html",
+        action="expert_revision",
+
     )
 
 
@@ -78,5 +101,6 @@ def send_expert_revision_file(user_id, file_url):
         user_id=user_id,
         theme='Revision request for File translation',
         template="expert_revision_email.html",
-        text=file_url,
+        file_url=file_url,
+        action="expert_revision"
     )
