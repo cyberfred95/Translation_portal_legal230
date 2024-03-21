cb061598eb94ac37d1cab8ef99c88af6f66f0779
Andrew Khlopot <a.xlopot@gmail.com>
2024-03-21 14:34:18 +0200

    add french name to language

--- languages/models.py
+++ languages/models.py
@@ -5,7 +5,7 @@ from django.utils.translation import gettext_lazy as _
 class Language(models.Model):
     name = models.CharField(verbose_name=_("Name"), max_length=255, blank=True)
     abbreviation = models.CharField(verbose_name=_("Abbreviation"), max_length=2, blank=True, null=True)
-
+    french_name = models.CharField(verbose_name=_("Name in French"), blank=True, null=True, max_length=255)
     class Meta:
         verbose_name = _("Language")
         verbose_name_plural = _("Languages")
