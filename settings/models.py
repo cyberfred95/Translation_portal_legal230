c83965616821155a8706ce9a8e67342bdc00ac02
Andrew Khlopot <a.xlopot@gmail.com>
2024-07-08 16:40:29 +0300

    Update models.py

--- settings/models.py
+++ settings/models.py
@@ -3,8 +3,14 @@ from preferences.models import Preferences
 
 
 class MainSettings(Preferences):
+
+    class AlgorithmChoices(models.TextChoices):
+        template = 'template', 'Template'
+        domains = 'domain', 'Domain'
+
     sender_email = models.EmailField()
     api_key = models.CharField(max_length=256)
+    algorithm = models.CharField(choices=AlgorithmChoices.choices, default=AlgorithmChoices.template, max_length=32)
 
     class Meta:
         verbose_name = "Main Settings"
