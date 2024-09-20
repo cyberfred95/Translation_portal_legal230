0d3708dd566738a055194a9449ddd07142a43e26
Andrew Khlopot <a.xlopot@gmail.com>
2024-09-20 16:24:02 +0300

    file_url

--- glossaries/views.py
+++ glossaries/views.py
@@ -12,7 +12,6 @@ class UserGlossariesView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['glossaries'] = self.get_glossaries()
-        print(context['glossaries'])
         return context
 
     def get_glossaries(self):
