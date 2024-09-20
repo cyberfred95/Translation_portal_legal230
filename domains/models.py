06d32fd7fe58187cab8a16a1feb67dff57e27e70
Andrew Khlopot <a.xlopot@gmail.com>
2024-09-20 16:19:40 +0300

    stabilize migrationss

--- domains/models.py
+++ domains/models.py
@@ -2,9 +2,15 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
+class DomainGroup(models.Model):
+    name = models.CharField(_("name"), max_length=255)
+    french_name = models.CharField(_("french_name"), max_length=255, blank=True, null=True)
+
+
 class Domain(models.Model):
     name = models.CharField(max_length=255)
     french_name = models.CharField(max_length=255, blank=True, null=True)
+    domain_group = models.ForeignKey(DomainGroup, on_delete=models.CASCADE, related_name='domains')
 
     class Meta:
         verbose_name = _("Domain")
