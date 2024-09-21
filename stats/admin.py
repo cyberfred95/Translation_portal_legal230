2cbe2b0e1b4b7716bb5fe6cc21cc24dec261f35a
Andrew Khlopot <a.xlopot@gmail.com>
2024-09-21 14:20:49 +0300

    remove statistic settings from the admin panel

--- stats/admin.py
+++ stats/admin.py
@@ -1,10 +1,3 @@
 from django.contrib import admin
 from .models import StatisticSettings
 from preferences.admin import PreferencesAdmin
-
-
-class StatisticSettingsAdmin(PreferencesAdmin):
-    exclude = ('sites',)
-
-
-admin.site.register(StatisticSettings, StatisticSettingsAdmin)
