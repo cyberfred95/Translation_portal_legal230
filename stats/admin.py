1ba09eab7b0eb4d7882e19db04de4ce23ed00514
Andrew Khlopot <a.xlopot@gmail.com>
2024-09-23 17:42:25 +0300

    set default value for statistic url

--- stats/admin.py
+++ stats/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 from .models import StatisticSettings
 from preferences.admin import PreferencesAdmin
+
+
+class SettingsAdmin(PreferencesAdmin):
+    exclude = ('sites', 'URL')
+
+admin.site.register(StatisticSettings, SettingsAdmin)
