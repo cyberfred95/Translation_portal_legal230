344dfc1c990f1af191d9478fc50725cf47703064
Andrew Khlopot <a.xlopot@gmail.com>
2024-09-24 11:00:43 +0300

    Update urls.py

--- legal/urls.py
+++ legal/urls.py
@@ -4,7 +4,6 @@ from django.urls import path, re_path
 from django.conf import settings
 from django.conf.urls.static import static, serve
 from .views import TranslateView, expert_revision, expert_revision_file, ProjectsHistoryView, UsageHistoryView, SingleProjectView, \
-
     GetTemplatesView, GetDomainsView, LanguageDetectView, DetectTextLanguageView
 from django.contrib.auth.decorators import login_required
 from django.conf.urls.i18n import i18n_patterns
