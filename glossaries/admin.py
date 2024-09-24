5201bf13ec596541400f94d29814ece0fba2ecf5
Andrew Khlopot <a.xlopot@gmail.com>
2024-09-24 20:38:19 +0300

    generate name from the file name, validate

--- glossaries/admin.py
+++ glossaries/admin.py
@@ -4,6 +4,7 @@ from .models import Glossary
 
 class GlossaryAdmin(admin.ModelAdmin):
     list_display = ('name', 'source_language', 'target_language', 'file_size')
+    exclude = ('name',)
 
 
 admin.site.register(Glossary, GlossaryAdmin)
