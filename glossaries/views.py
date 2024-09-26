db1780192d3841e740195df6cd24ca78ed05d4ed
Andrew Khlopot <a.xlopot@gmail.com>
2024-09-26 21:32:27 +0300

    Update views.py

--- glossaries/views.py
+++ glossaries/views.py
@@ -20,6 +20,7 @@ class UserGlossariesView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['glossaries'] = self.get_glossaries()
+        context['translate_languages'] = Language.objects.all()
         return context
 
     def get_glossaries(self):
