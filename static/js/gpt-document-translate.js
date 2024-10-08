c865450feacde14b2db181098f101baad71a75f6
Hropotova <vitaliyvovchok0815@gmail.com>
2024-10-08 17:38:22 +0300

    FIX: Added writing

--- static/js/gpt-document-translate.js
+++ static/js/gpt-document-translate.js
@@ -1,9 +1,38 @@
-$(document).ready(function () {
+$(document).ready(function() {
     $(".actions-select").attr("data-placeholder", "Action");
-    $(".actions-select").select2();
 
-    $select = $(".actions-select").select2();
+    $('.actions-select').select2({
+        templateResult: formatOption,
+        templateSelection: formatSelection,
+        escapeMarkup: function(m) { return m; }
+    });
 
+    var $select = $(".actions-select");
     $select.data('select2').$container.addClass('domain');
     $select.data('select2').$dropdown.addClass('domain');
+
+    function formatOption(option) {
+        if (!option.id) {
+            return option.text;
+        }
+        var splitText = option.text.split('::');
+        if (splitText.length < 2) {
+            return option.text;
+        }
+        var $option = $(
+            '<div class="flex flex-col gap-1.5">' +
+            '<div class="font-medium text-4">' + splitText[0] + '</div>' +
+            '<div class="font-normal text-3.5 text-gray-590">' + splitText[1] + '</div>' +
+            '</div>'
+        );
+        return $option;
+    }
+
+    function formatSelection(option) {
+        if (!option.id) {
+            return option.text;
+        }
+        var splitText = option.text.split('::');
+        return splitText[0];
+    }
 });
