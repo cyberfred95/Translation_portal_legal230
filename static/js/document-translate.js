d919c3bdebba5c4ed3e62ac2ecdef3d00f5cd4da
Hropotova <vitaliyvovchok0815@gmail.com>
2024-11-18 10:29:11 +0200

    FEAT: Changed colors for document translate

--- static/js/document-translate.js
+++ static/js/document-translate.js
@@ -9,6 +9,10 @@ $(document).ready(function () {
     let glossaryFile = '';
     let selectedFiles = [];
 
+    const nextStep = $("#next-step");
+    const prevStep = $("#prev-step");
+
+
     // ------------- PROGRESS BAT -------------
 
 
@@ -60,37 +64,37 @@ $(document).ready(function () {
         const $actionList = $(".action-list");
 
         if (step === 0) {
-            $("#prev-step").hide();
+            prevStep.hide();
             $("#restart").hide();
             $("#restart-text").hide();
 
             if (selectedFiles.length > 0) {
-                $("#next-step").show().text(language_code === 'en' ? "Next" : "Suivant");
+                nextStep.show().text(language_code === 'en' ? "Next" : "Suivant");
                 $actionList.css("justify-content", "flex-end");
             } else {
-                $("#next-step").hide();
+                nextStep.hide();
             }
         } else if (currentStep === 4) {
-            $("#prev-step").hide();
-            $("#next-step").hide();
+            prevStep.hide();
+            nextStep.hide();
             $("#restart").show().text(language_code === 'en' ? "New translation" : "Nouveau document");
             $("#restart-text").show();
 
             $actionList.css("justify-content", "flex-start");
         } else {
-            $("#prev-step").show();
-            $("#next-step").show().text(language_code === 'en' ? "Next" : "Suivant");
+            prevStep.show();
+            nextStep.show().text(language_code === 'en' ? "Next" : "Suivant");
             $("#restart").hide();
             $("#restart-text").hide();
 
             $actionList.css("justify-content", "space-between");
         }
 
-        $("#prev-step").toggleClass('hidden', step === 0);
-        $("#next-step").toggleClass('hidden', step === 4);
+        prevStep.toggleClass('hidden', step === 0);
+        nextStep.toggleClass('hidden', step === 4);
     }
 
-    $("#next-step").click(function () {
+    nextStep.click(function () {
         if (currentStep === 0 && selectedFiles.length === 0) {
             return;
         }
@@ -120,11 +124,11 @@ $(document).ready(function () {
         showStep(currentStep);
     });
 
-    $("#prev-step").click(function () {
+    prevStep.click(function () {
         if (currentStep > 0) {
             currentStep--;
             showStep(currentStep);
-            $("#next-step").removeClass('border-gray-225 text-gray-225 pointer-events-none').addClass('border-green-400 text-green-400').prop("disabled", false);
+            nextStep.removeClass('border-gray-225 text-gray-225 pointer-events-none').addClass('border-green-700 text-green-700').prop("disabled", false);
 
             $('.step-container').removeClass('bg-red-100 border-red-200');
         }
@@ -238,14 +242,14 @@ $(document).ready(function () {
         const $actionList = $(".action-list");
 
         if (filesExist && currentStep === 0) {
-            $("#prev-step").hide();
-            $("#next-step").show().text(language_code === 'en' ? "Next" : "Suivant");
+            prevStep.hide();
+            nextStep.show().text(language_code === 'en' ? "Next" : "Suivant");
             $("#restart").hide();
             $("#restart-text").hide();
 
             $actionList.css("justify-content", "flex-end");
         } else if (currentStep === 0) {
-            $("#next-step").hide();
+            nextStep.hide();
         }
 
         $fileList.toggleClass('hidden', !filesExist);
@@ -255,7 +259,6 @@ $(document).ready(function () {
         const isPdf = files.some(file => file.name.toLowerCase().endsWith('.pdf'));
         $(".pdf-document").toggleClass('hidden', !isPdf);
     }
-
 
     function removeFile(fileId) {
         selectedFiles = selectedFiles.filter(file => file.fileId !== fileId);
@@ -519,12 +522,12 @@ $(document).ready(function () {
         domains.forEach((domain, index) => {
             const button = $('<button>', {
                 type: 'button',
-                class: 'domain-button text-3.5 py-3 px-7.5 bg-gray-100 text-gray-475 hover:bg-green-400 hover:text-white rounded-md focus:text-white focus:bg-green-400 transition duration-300 ease-in-out',
+                class: 'domain-button text-3.5 py-3 px-7.5 bg-gray-100 text-gray-475 hover:bg-green-700 hover:text-white rounded-md focus:text-white focus:bg-green-700 transition duration-300 ease-in-out',
                 text: domain.name,
                 'data-name': domain.name,
                 click: function () {
-                    $('.domain-button').removeClass('selected bg-green-400 text-white').addClass('bg-gray-100 text-gray-475');
-                    $(this).removeClass('bg-gray-100 text-gray-475').addClass('selected bg-green-400 text-white');
+                    $('.domain-button').removeClass('selected bg-green-700 text-white').addClass('bg-gray-100 text-gray-475');
+                    $(this).removeClass('bg-gray-100 text-gray-475').addClass('selected bg-green-700 text-white');
 
                     selectedDomain = $(this).data('name');
                     getDomains();
@@ -532,7 +535,7 @@ $(document).ready(function () {
             });
 
             if (index === 0) {
-                button.removeClass('bg-gray-100 text-gray-475').addClass('selected bg-green-400 text-white');
+                button.removeClass('bg-gray-100 text-gray-475').addClass('selected bg-green-700 text-white');
 
                 selectedDomain = domain.name;
             }
@@ -550,19 +553,19 @@ $(document).ready(function () {
         subDomains.forEach((subDomain, index) => {
             const button = $('<button>', {
                 type: 'button',
-                class: 'sub-domain-button text-3.5 py-3 px-7.5 bg-gray-175 text-gray-375 hover:bg-green-500 hover:text-white rounded-md focus:text-white focus:bg-green-500 transition duration-300 ease-in-out',
+                class: 'sub-domain-button text-3.5 py-3 px-7.5 bg-gray-175 text-gray-375 hover:bg-green-700 hover:text-white rounded-md focus:text-white focus:bg-green-700 transition duration-300 ease-in-out',
                 text: subDomain,
                 'data-name': subDomain,
                 click: function () {
                     $('.sub-domain-button').removeClass('selected bg-green-500 text-white').addClass('bg-gray-175 text-gray-375');
-                    $(this).removeClass('bg-gray-175 text-gray-375').addClass('selected bg-green-500 text-white');
+                    $(this).removeClass('bg-gray-175 text-gray-375').addClass('selected bg-green-700 text-white');
                     selectedSubDomain = $(this).data('name');
                     $('.domain-step').text(selectedSubDomain).removeClass('hidden');
                 }
             });
 
             if (index === 0) {
-                button.removeClass('bg-gray-175 text-gray-375').addClass('selected bg-green-500 text-white');
+                button.removeClass('bg-gray-175 text-gray-375').addClass('selected bg-green-700 text-white');
                 selectedSubDomain = subDomain;
             }
 
@@ -610,14 +613,14 @@ $(document).ready(function () {
                 }
 
                 if (response.data && response.data.length === 0) {
-                    $('#next-step').removeClass('border-green-400 text-white text-green-400')
+                    nextStep.removeClass('border-green-700 text-white text-green-700')
 
                         .addClass('border-gray-225 text-gray-225 pointer-events-none')
                         .prop("disabled", true);
                     $('.domain-step').text(language_code === 'en' ? 'None' : 'Aucun').removeClass('hidden');
                 } else {
-                    $('#next-step').removeClass('border-gray-225 text-gray-225 pointer-events-none')
-                        .addClass('border-green-400 text-green-400')
+                    nextStep.removeClass('border-gray-225 text-gray-225 pointer-events-none')
+                        .addClass('border-green-700 text-green-700')
                         .prop("disabled", false);
                     $('.domain-step').text(response.data[0]).removeClass('hidden');
                 }
@@ -650,8 +653,8 @@ $(document).ready(function () {
         selectGlossaryType('none');
         $('.terminology-step').text(language_code === 'en' ? 'none' : 'aucun').removeClass('hidden');
         selectedGlossary = 'none';
-        $('#next-step').removeClass('border-gray-225 text-gray-225 pointer-events-none')
-            .addClass('border-green-400 text-green-400')
+        nextStep.removeClass('border-gray-225 text-gray-225 pointer-events-none')
+            .addClass('border-green-700 text-green-700')
             .prop("disabled", false);
         clearGlossaryList();
     });
@@ -693,8 +696,8 @@ $(document).ready(function () {
             },
             error: function () {
                 errorNotification();
-                $('#next-step').removeClass('border-gray-225 text-gray-225 pointer-events-none')
-                    .addClass('border-green-400 text-green-400')
+                nextStep.removeClass('border-gray-225 text-gray-225 pointer-events-none')
+                    .addClass('border-green-700 text-green-700')
                     .prop("disabled", false);
             }
         });
@@ -719,7 +722,7 @@ $(document).ready(function () {
                 updateGlossaryList(response, false);
                 selectedGlossary = '';
                 $('.terminology-step').text('default').removeClass('hidden');
-                $('#next-step').removeClass('border-green-400 text-white text-green-400')
+                nextStep.removeClass('border-green-700 text-white text-green-700')
                     .addClass('border-gray-225 text-gray-225 pointer-events-none')
                     .prop("disabled", true);
             },
@@ -736,26 +739,26 @@ $(document).ready(function () {
         $list.empty();
 
         glossaries.forEach(function (glossary) {
-            const $item = $(`<button type="button" class="glossary-item text-3.5 py-3 px-7.5 ${isDefault ? "bg-green-500 text-white" : "bg-gray-175 text-gray-375"} rounded-md hover:bg-green-500 hover:text-white transition duration-300 ease-in-out">${glossary.name}</button>`);
+            const $item = $(`<button type="button" class="glossary-item text-3.5 py-3 px-7.5 ${isDefault ? "bg-green-700 text-white" : "bg-gray-175 text-gray-375"} rounded-md hover:bg-green-700 hover:text-white transition duration-300 ease-in-out">${glossary.name}</button>`);
             $item.click(function () {
                 if (selectedGlossary === glossary.id) {
-                    $(this).removeClass('bg-green-500 text-white').addClass('bg-gray-175 text-gray-375');
+                    $(this).removeClass('bg-green-700 text-white').addClass('bg-gray-175 text-gray-375');
                     selectedGlossary = '';
                     $('.terminology-step').text('').removeClass('hidden');
                     if (selectedGlossaryType === 'my-glossary') {
-                        $('#next-step').removeClass('border-green-400 text-white text-green-400')
+                        nextStep.removeClass('border-green-700 text-white text-green-700')
                             .addClass('border-gray-225 text-gray-225 pointer-events-none')
                             .prop("disabled", true);
                     }
                 } else {
-                    $(".glossary-item").removeClass('bg-green-500 text-white').addClass('bg-gray-175 text-gray-375');
-                    $(this).removeClass('bg-gray-175 text-gray-375').addClass('bg-green-500 text-white');
+                    $(".glossary-item").removeClass('bg-green-700 text-white').addClass('bg-gray-175 text-gray-375');
+                    $(this).removeClass('bg-gray-175 text-gray-375').addClass('bg-green-700 text-white');
                     selectedGlossary = glossary.id;
                     $('.terminology-step').text(selectedGlossary).removeClass('hidden');
 
                     if (selectedGlossaryType === 'my-glossary') {
-                        $('#next-step').removeClass('border-gray-225 text-gray-225 pointer-events-none')
-                            .addClass('border-green-400 text-green-400')
+                        nextStep.removeClass('border-gray-225 text-gray-225 pointer-events-none')
+                            .addClass('border-green-700 text-green-700')
                             .prop("disabled", false);
                     }
                 }
@@ -871,7 +874,7 @@ $(document).ready(function () {
                         $(this).removeClass('bg-green-500 text-white').addClass('bg-gray-175 text-gray-375');
                         selectedGlossary = '';
                         $('.terminology-step').text('').removeClass('hidden');
-                        $('#next-step').removeClass('border-green-400 text-green-400 ')
+                        nextStep.removeClass('border-green-700 text-green-700 ')
                             .addClass('border-gray-225 text-gray-225 pointer-events-none')
                             .prop("disabled", true);
                     } else {
@@ -879,8 +882,8 @@ $(document).ready(function () {
                         $(this).removeClass('bg-gray-175 text-gray-375').addClass('bg-green-500 text-white');
                         selectedGlossary = response.id;
                         $('.terminology-step').text(selectedGlossary).removeClass('hidden');
-                        $('#next-step').removeClass('border-gray-225 text-gray-225 pointer-events-none')
-                            .addClass('border-green-400 text-green-400')
+                        nextStep.removeClass('border-gray-225 text-gray-225 pointer-events-none')
+                            .addClass('border-green-700 text-green-700')
                             .prop("disabled", false);
                     }
                 });
