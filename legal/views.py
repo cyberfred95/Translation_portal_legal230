cede87b8c6b3efd2a8a5f7981f2358cd5d18646a
Andrew Khlopot <a.xlopot@gmail.com>
2024-11-28 12:29:34 +0200

    Update views.py

--- legal/views.py
+++ legal/views.py
@@ -316,10 +316,7 @@ class LanguageDetectView(APIView):
             api_key = preferences.MainSettings.api_key if request.user.is_staff else request.user.group.api_key
             text_for_detection = self.get_text_for_detection(api_key=api_key, file=file)
             try:
-                print(text_for_detection)
-
                 tmp_language = langdetect.detect(text_for_detection)
-                print(tmp_language)
                 language = Language.objects.filter(abbreviation__exact=tmp_language.upper()).values_list(
                     'abbreviation', flat=True).first()
             except langdetect.LangDetectException:
