76f8988b8effb0306337f5c64fcf572fa89fb602
Andrew Khlopot <a.xlopot@gmail.com>
2024-12-03 18:06:42 +0200

    Update serializers.py

--- domains/serializers.py
+++ domains/serializers.py
@@ -17,7 +17,6 @@ class DomainSerializer(serializers.ModelSerializer):
 
 
 class DomainGroupSerializer(serializers.ModelSerializer):
-    domains = serializers.SerializerMethodField()
     name = serializers.SerializerMethodField()
 
     def get_name(self, obj: DomainGroup):
@@ -25,12 +24,6 @@ class DomainGroupSerializer(serializers.ModelSerializer):
             return obj.french_name
         return obj.name
 
-    @staticmethod
-    def get_domains(obj: DomainGroup):
-        domains = obj.domains.all()
-        if domains.count() == 0 and preferences.DefaultTranslation.enabled:
-            return DefaultDomainSerializer(preferences.DefaultTranslation, many=False).data
-        return DomainSerializer(domains, many=True).data
 
     class Meta:
         model = DomainGroup
