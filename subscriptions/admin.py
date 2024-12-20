79534d355d40ebdc97d18bcb7016870d3f69c96f
Andrew Khlopot <a.xlopot@gmail.com>
2024-12-20 13:59:53 +0200

    display group subscriptions in admin panel

--- subscriptions/admin.py
+++ subscriptions/admin.py
@@ -7,10 +7,12 @@ admin.site.register(SubscriptionType)
 
 class GroupSubscriptionAdmin(admin.ModelAdmin):
     fieldsets = (
-        (None, {'fields': ('group', 'subscription')}),
-        ('Allowed', {'fields': ('max_words_count', 'custom_glossaries_count', )}),
-        ('Used', {'fields': ('used_words_count',)}),
-        ('Access permissions', {'fields': ('access_to_writing', 'access_to_official_glossaries', 'access_to_sso')})
+        (None, {'fields': ('group', 'subscription', 'status')}),
+        ('Allowed', {'fields': ('max_words_count', 'max_files_count', 'custom_glossaries_count',)}),
+        ('Translated', {'fields': ('translated_words_count', 'translated_files_count')}),
+        ('Access permissions', {'fields': ('access_to_writing', 'access_to_official_glossaries', 'access_to_sso')}),
+        ('Dates', {'fields': ('start_date', 'end_date')}),
     )
+
 
 admin.site.register(GroupSubscription, GroupSubscriptionAdmin)
