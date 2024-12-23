a8ff695a33021fddd6c195f9b2abfe0f8c26149a
Andrew Khlopot <a.xlopot@gmail.com>
2024-12-23 15:17:10 +0200

    update permission

--- subscriptions/permissions.py
+++ subscriptions/permissions.py
@@ -1,5 +1,6 @@
 from rest_framework import permissions
-
+from .models import GroupSubscription
+from django.utils.timezone import datetime
 
 class SubscribedPermission(permissions.BasePermission):
 
@@ -11,6 +12,12 @@ class SubscribedPermission(permissions.BasePermission):
         if not request.user.is_staff:
             if not request.user.group:
                 return False
-            if not request.user.group.subscriptions.first():
+            subscription = request.user.group.subscriptions.first()
+            if not subscription:
                 return False
+            if subscription.status != GroupSubscription.GroupSubscriptionChoices.ACTIVE:
+                return False
+            if datetime.now() > subscription.end_date or datetime.now() < subscription.start_date:
+                return False
+
         return True
