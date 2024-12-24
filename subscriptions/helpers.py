30ff10ad6485028c1e7a619f2968a3c239cacb33
Andrew Khlopot <a.xlopot@gmail.com>
2024-12-24 15:27:44 +0200

    Update helpers.py

--- subscriptions/helpers.py
+++ subscriptions/helpers.py
@@ -1,7 +1,8 @@
 def translation_allowed(request, words_count: int, files_count: int = None) -> bool:
+    if request.user.is_staff:
+        return True
+
     group_subscription = request.user.group.subscriptions.first()
-    print(files_count)
-    print(words_count)
     if files_count:
         if group_subscription.max_files_count < 0 and group_subscription.max_files_count < 0:
             return True
@@ -17,8 +18,9 @@ def translation_allowed(request, words_count: int, files_count: int = None) -> b
 
 
 def add_translations(request, words_count: int, files_count: int = None):
-    group_subscription = request.user.group.subscriptions.first()
-    group_subscription.translated_words_count += words_count
-    if files_count:
-        group_subscription.translated_files_count += files_count
-    group_subscription.save()
+    if not request.user.is_staff:
+        group_subscription = request.user.group.subscriptions.first()
+        group_subscription.translated_words_count += words_count
+        if files_count:
+            group_subscription.translated_files_count += files_count
+        group_subscription.save()
