3ee85b8358fb647611a95c98c2b31371ab313c06
Andrew Khlopot <a.xlopot@gmail.com>
2025-03-06 15:15:31 +0200

    add login required

--- users/urls.py
+++ users/urls.py
@@ -5,9 +5,9 @@ from .views import UsersListView, DeleteAllDataView, SingleAccountView, ChangePa
 
 urlpatterns = [
     path('', login_required(UsersListView.as_view()), name='groups'),
-    path('delete-all-data/', DeleteAllDataView.as_view(), name='delete_all_data'),
-    path('change-password/', ChangePasswordView.as_view(), name='change_password'),
-    path('<int:id>/', SingleAccountView.as_view(), name='user'),
+    path('delete-all-data/', login_required(DeleteAllDataView.as_view()), name='delete_all_data'),
+    path('change-password/', login_required(ChangePasswordView.as_view()), name='change_password'),
+    path('<int:id>/', login_required(SingleAccountView.as_view()), name='user'),
     path('register/', RegisterUserView.as_view(), name='register-user'),
     path('invite/', login_required(InviteUserAPIView.as_view()), name='invite-user'),
     path('login/', LoginView.as_view(), name='login'),
--- writing/urls.py
+++ writing/urls.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 from .views import WritingView, WritingProcessAPIView
 
 urlpatterns = [
-    path('', WritingView.as_view(), name='writing'),
-    path('process/', WritingProcessAPIView.as_view(), name='process'),
+    path('', login_required(WritingView.as_view()), name='writing'),
+    path('process/', login_required(WritingProcessAPIView.as_view()), name='process'),
 ]
