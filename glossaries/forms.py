f90ebf5028c674ff24c8540db81f082828b46f41
Andrew Khlopot <a.xlopot@gmail.com>
2025-04-10 15:56:54 +0300

    allow blank lines for xlsx, add validation from admin page

---
+++ glossaries/forms.py
@@ -0,0 +1,17 @@
+from django.forms import ModelForm
+from .models import Glossary
+from .processor import GlossaryProcessor
+
+
+class GlossaryAdminForm(ModelForm):
+    class Meta:
+
+        model = Glossary
+        fields = '__all__'
+
+    def clean_file(self):
+        processor = GlossaryProcessor()
+        print("CLEANED FILE")
+        print(self.cleaned_data['file'])
+        processor.validate_file(self.cleaned_data['file'])
+
