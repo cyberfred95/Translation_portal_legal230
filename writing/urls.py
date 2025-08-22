a747c269b862ec570c0ac203e8d8742eebdacc1e
pbernier <pacome@lecart.dev>
2025-08-22 17:22:37 +0200

    feat (front): Add the Writing2 view and the interface template for the writing tools.

--- writing/urls.py
+++ writing/urls.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.urls import path
-from .views import WritingView, WritingProcessAPIView
+from .views import WritingView, WritingProcessAPIView, Writing2View
 
 urlpatterns = [
     path('', login_required(WritingView.as_view()), name='writing'),
+    path('writing_2/', login_required(Writing2View.as_view()), name='writing_2'),
     path('process/', login_required(WritingProcessAPIView.as_view()), name='process'),
 ]
