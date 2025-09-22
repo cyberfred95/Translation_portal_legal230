c0ce5539549e485c1e7583a0ade031f3e4d99871
plecart <pacome@lecart.dev>
2025-09-22 12:28:51 +0200

    refactor(profile) : Updated user language management logic in the profile by replacing session language with user preference language.

--- users/views.py
+++ users/views.py
@@ -115,14 +115,16 @@ class SingleAccountView(RetrieveUpdateDestroyAPIView):
         user.email = request.data['email']
         user.first_name = request.data.get('first_name', user.first_name)
         user.last_name = request.data.get('last_name', user.last_name)
-        user.save()
         
-        # Mettre à jour la langue de session si fournie
+        # Mettre à jour la langue de préférence de l'utilisateur
         if 'language' in request.data:
+            user.language = request.data['language']
+            # Mettre à jour aussi la langue de session pour l'interface actuelle
             from django.utils import translation
             translation.activate(request.data['language'])
             request.session[translation.LANGUAGE_SESSION_KEY] = request.data['language']
         
+        user.save()
         return Response(UserSerializer(user).data, status=status.HTTP_200_OK)
 
     def get_object(self):
