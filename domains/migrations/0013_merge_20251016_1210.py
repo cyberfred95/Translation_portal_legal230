9bd7bf268f26193d20742e3f13dd5545613d4e76
pbernier <pacome@lecart.dev>
2025-10-16 15:53:01 +0200

    feat(admin): add icon display functionality in PromptAdmin

---
+++ domains/migrations/0013_merge_20251016_1210.py
@@ -0,0 +1,13 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('domains', '0012_remove_domain_icon_weight'),
+        ('domains', '0012_add_default_domain_entities'),
+    ]
+
+    operations = []
+
+
