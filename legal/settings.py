1ab52b44bad0aa79060d95706554852995ecdd75
pbernier <pacome@lecart.dev>
2025-11-13 14:42:30 +0100

    feat(settings): Enable ManifestStaticFilesStorage for production environments

--- legal/settings.py
+++ legal/settings.py
@@ -159,6 +159,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, "static") ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_collected')
 
+if not DEBUG:
+    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
