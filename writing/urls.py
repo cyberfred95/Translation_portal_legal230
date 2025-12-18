bb37798fb7ab2315ecfb712de2c12253d3b8216d
Frederic Bernier <frederic.bernier@legal230.com>
2025-12-18 12:31:42 +0100

    fix(sidebar): Update writing URL namespace for consistency

--- writing/urls.py
+++ writing/urls.py
@@ -2,6 +2,8 @@ from django.contrib.auth.decorators import login_required
 from django.urls import path
 from .views import WritingView, WritingProcessAPIView
 
+app_name = 'writing'
+
 urlpatterns = [
     path('', login_required(WritingView.as_view()), name='writing'),
     path('process/', login_required(WritingProcessAPIView.as_view()), name='process'),
